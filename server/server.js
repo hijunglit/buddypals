c2fe8f398fa1ca25a05d04200aa75e889920085e
hijunglit <hijunglit@gmail.com>
2024-05-28 17:38:10 +0900

    router plan

--- server/server.js
+++ server/server.js
@@ -2,13 +2,30 @@ import "./db.js";
 import express from "express";
 import cors from "cors";
 import records from "./routes/record.js";
+import morgan from "morgan";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
+const globalRouter = express.Router();
+const userRouter = express.Router();
+const postRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+const handleEditUser = (req, res) => res.send("Edit user");
+const handleSeePost = (req, res) => res.send("See post");
+
+globalRouter.get("/", handleHome);
+userRouter.get("/edit", handleEditUser);
+postRouter.get("/see", handleSeePost);
+
 app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"));
+app.use("/", globalRouter);
 app.use("/record", records);
+app.use("/users", userRouter);
+app.use("posts", postRouter);
 
 const handleListening = () =>
   console.log(`✅ Server listening on http://localhost:${PORT}`);
